4be277639e9a9179218bab3fdcad84eca2efdb33
tschmidty69 <tschmidty@yahoo.com>
2018-04-15 16:40:59 -0400

    updates

--- snips/customtts.sh
+++ snips/customtts.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-PLATFORM=$1
-FILE=$2
-LANG=$3
-TEXT=$4
+API_KEY=$1
+PLATFORM=$2
+FILE=$3
+LANG=$4
+TEXT=$5
 
 if [ -z "$LANG" || $LANG == "null" ]; then
     LANG="en-US"
 fi
 
-RESPONSE=`curl -s -H "Type: application/json" http://hassio.local:8123/api/tts_get_url -d '{"message": "$TEXT", "platform": "$PLATFORM", "language": "$LANG"}'`
+RESPONSE=`curl -s -H "x-ha-access: $API_KEY" -H "Type: application/json" http://hassio.local:8123/api/tts_get_url -d '{"message": "$TEXT", "platform": "$PLATFORM", "language": "$LANG"}'`
 echo $RESPONSE
 URL=`echo $RESPONSE | jq --raw-output '.url'`
 
