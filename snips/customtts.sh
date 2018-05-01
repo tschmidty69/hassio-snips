ffa8ebed3cf4ba130e8591d6679e4022a1b80009
tschmidty69 <tschmidty@yahoo.com>
2018-05-01 16:06:13 -0400

    updated version

--- snips/customtts.sh
+++ snips/customtts.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -x
 API_KEY=$1
 PLATFORM=$2
 FILE=$3
@@ -7,11 +8,13 @@ LANG=$4
 TEXT=$5
 
 MESSAGE="'{\"message\": \"$TEXT\", \"platform\": \"$PLATFORM\"}'"
+echo $MESSAGE
 
-RESPONSE=$(eval curl -s -H \"x-ha-access: $API_KEY\" -H \"Type: application/json\" http://localhost:8123/api/tts_get_url -d $MESSAGE)
+RESPONSE=$(eval curl -s -H \"x-ha-access: $API_KEY\" -H \"Type: application/json\" http://hassio/homeassistant/api/tts_get_url -d $MESSAGE)
 if [ "$RESPONSE" = "" ]; then
     exit 1
 fi
+echo $RESPONSE
 
 URL=`echo $RESPONSE | jq --raw-output '.url'`
 if [ "$URL" = "" ]; then
@@ -19,7 +22,7 @@ if [ "$URL" = "" ]; then
 fi
 
 rm /tmp/temp.mp3
-curl -s -H "x-ha-access: $API_KEY" "$URL" -s -o /tmp/temp.mp3
+curl -s -H "x-ha-access: $API_KEY" "$URL" -o /tmp/temp.mp3
 if [ -f /tmp/temp.mp3 ]; then
   /usr/bin/mpg123 -w $FILE /tmp/temp.mp3
 fi
