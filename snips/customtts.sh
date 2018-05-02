ae1763db424881ee299e0d5d10cb3c1eae8ccf55
tschmidty69 <tschmidty@yahoo.com>
2018-05-02 13:52:14 -0400

    updated version

--- snips/customtts.sh
+++ snips/customtts.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#set -x
+set -x
 API_KEY=$HASSIO_TOKEN
 PLATFORM=$1
 FILE=$2
 LANG=$3
 TEXT=$4
 
-MESSAGE="\"{\"message\": \"$TEXT\", \"platform\": \"$PLATFORM\"}\""
-echo $MESSAGE
+MESSAGE="\"{\\\"message\\\": \\\"$TEXT\\\", \\\"platform\\\": \\\"$PLATFORM\\\"}\""
+echo "$MESSAGE"
 
 RESPONSE=$(eval curl -s -H \"x-ha-access: $API_KEY\" -H \"Type: application/json\" http://hassio/homeassistant/api/tts_get_url -d $MESSAGE)
 if [ "$RESPONSE" = "" ]; then
@@ -16,7 +16,7 @@ if [ "$RESPONSE" = "" ]; then
 fi
 echo $RESPONSE
 
-URL=`echo $RESPONSE | jq --raw-output '.url'`
+URL=$(echo "$RESPONSE" | jq --raw-output '.url')
 if [ "$URL" = "" ]; then
     exit 1
 fi
@@ -24,7 +24,7 @@ fi
 rm /tmp/temp.mp3
 curl -s -H "x-ha-access: $API_KEY" "$URL" -o /tmp/temp.mp3
 if [ -f /tmp/temp.mp3 ]; then
-  /usr/bin/mpg123 -w $FILE /tmp/temp.mp3
+  /usr/bin/mpg123 -w "$FILE" /tmp/temp.mp3
 fi
 rm /tmp/temp.mp3
 
